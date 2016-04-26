684a6b30129374f52ce63ca74bbdf9f46b356671
zhoubinwy <zhoubinwy@gmail.com>
2016-04-26 18:09:20 -0400

    Use TYPE_ROTATION_VECTOR instead of  the depreciated TYPE_ORIENTATION

--- app/src/main/java/com/example/susha/sensor_collect/Sensors/LogRunnable.java
+++ app/src/main/java/com/example/susha/sensor_collect/Sensors/LogRunnable.java
@@ -58,8 +58,8 @@ public class LogRunnable implements Runnable{
             currentDevice.add(mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY));
         if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null)
             currentDevice.add(mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE));
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION) != null)
-            currentDevice.add(mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION));
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) != null)
+            currentDevice.add(mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR));
         if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null)
             currentDevice.add(mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD));
         /*mHandlerThread = new HandlerThread("InertiaLogListener");
@@ -100,9 +100,25 @@ public class LogRunnable implements Runnable{
                     }
                 }
 
-                if (event.sensor == mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION) && !fileHandler.isOrientationStreamNull()) {// && !lowAccuracy[currentDevice.indexOf
+                if (event.sensor == mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) && !fileHandler.isOrientationStreamNull()) {// && !lowAccuracy[currentDevice.indexOf
                     //(event.sensor)]) {
-                    String add = System.currentTimeMillis() + "\t" + event.values[0] + "\t" + event.values[1] + "\t" + event.values[2] + "\n";
+                    // Convert the rotation-vector to a 4x4 matrix.
+                    float[] mRotationMatrix = new float[16];
+                   // float[] mRotationMatrixFromVector = new float[16];
+                    SensorManager.getRotationMatrixFromVector(mRotationMatrix,event.values);
+                   // SensorManager.remapCoordinateSystem(mRotationMatrixFromVector,SensorManager.AXIS_X, SensorManager.AXIS_Z,
+                   //                 mRotationMatrix);
+
+                    float[] orientationVals = new float[3];
+                    SensorManager.getOrientation(mRotationMatrix, orientationVals);
+
+                    // Optionally convert the result from radians to degrees
+                    orientationVals[0] = (float) Math.toDegrees(orientationVals[0]);
+                    orientationVals[1] = (float) Math.toDegrees(orientationVals[1]);
+                    orientationVals[2] = (float) Math.toDegrees(orientationVals[2]);
+
+                    //String add = System.currentTimeMillis() + "\t" + event.values[0] + "\t" + event.values[1] + "\t" + event.values[2] + "\n";
+                    String add = System.currentTimeMillis() + "\t" + orientationVals[0] + "\t" + orientationVals[1] + "\t" + orientationVals[2] + "\n";
                     try {
                         fileHandler.writeOrientation(add);
                     } catch (IOException e) {
